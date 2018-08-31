0e62784016f060ebad82ff2c16448743d062ca01
Michail Pevnev <mpevnev@gmail.com>
2018-09-01 00:44:06 +0300

    Implement first iteration of Newton's method

--- src/roots.rs
+++ src/roots.rs
@@ -145,6 +145,102 @@ impl<'a, T, F> Iterator for MultiRootBisectState<'a, T, F>
     }
 }
 
+/* ---------- Newton's method ---------- */
+
+/// Configuration structure for the Newton's method (one root version).
+#[derive(Debug, Clone, Copy)]
+pub struct OneRootNewtonCfg<T> {
+    /// The real root, if any, is most likely to be within this distance from
+    /// the reported root, but this is not guaranteed.
+    pub precision: T,
+    /// A limit on the number of iterations to perform. Pass `None` if you
+    /// don't want a limit.
+    pub max_iters: Option<u32>
+}
+
+pub fn newton_one<T, F, D>(config: OneRootNewtonCfg<T>,
+                           left: T,
+                           right: T,
+                           first_approx: T,
+                           target: &F,
+                           derivative: &D)
+    -> Option<T>
+    where T: Float + Epsilon<RHS=T, Precision=T>,
+          F: Fn(T) -> T,
+          D: Fn(T) -> T
+{
+    let mut left = left;
+    let mut right = right;
+    let mut root = first_approx;
+    let mut prev_root = None;
+    let mut iter = 0;
+    while prev_root.map_or(true, |old| root.close(old, config.precision))
+        && config.max_iters.map_or(true, |max| iter < max) {
+            iter += 1;
+            let left_val = target(left);
+            let right_val = target(right);
+            if let Some(next) = next_newton_iter(config.precision,
+                                                 left, 
+                                                 right, 
+                                                 root, 
+                                                 target, 
+                                                 derivative) {
+                prev_root = Some(root);
+                root = next;
+            } else if let Some(fallback_root) 
+                = linear_fallback(left, right, left_val, right_val) {
+                    prev_root = Some(root);
+                    root = fallback_root;
+            } else {
+                return None
+            }
+            let val_at_root = target(root);
+            if left_val * val_at_root <= T::zero() {
+                right = root;
+            } else {
+                left = root;
+            }
+    }
+    Some(root)
+}
+
+fn next_newton_iter<T, F, D>(prec: T,
+                             left: T,
+                             right: T,
+                             old: T,
+                             target: &F,
+                             derivative: &D)
+    -> Option<T>
+    where T: Float + Epsilon<RHS=T, Precision=T>,
+          F: Fn(T) -> T,
+          D: Fn(T) -> T
+{
+    let d = derivative(old);
+    if d.near_zero(prec) {
+        return None
+    }
+    let res = old - target(old) / d;
+    if res < left {
+        None
+    } else if res > right {
+        None
+    } else {
+        Some(res)
+    }
+}
+
+fn linear_fallback<T: Float>(x1: T , x2: T, y1: T, y2: T) -> Option<T>
+{
+    let res = ((y2 - y1) * x1 - (x2 - x1) * y1) / (y2 - y1);
+    if res < x1 {
+        None
+    } else if res > x2 {
+        None
+    } else {
+        Some(res)
+    }
+}
+
 /* ---------- unit tests ---------- */
 
 #[cfg(test)]
